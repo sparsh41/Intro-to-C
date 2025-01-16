a21b0e5c823432d93215a623c370ff955aec6a36
sparsh41 <snrang34@gmail.com>
2025-01-16 16:26:33 +0000

    Implemented first program

---
+++ 01_first_progam/first_progam.c
@@ -0,0 +1,16 @@
+// One line comment
+// This is the first program which prints a welcoming message
+/*
+    Multi-line comment
+    January 9, 2025
+*/
+
+#include <stdio.h>
+
+//Create a main function
+int main() {
+    printf("Welcome\nto\nCOP2271!\n");
+    printf("I\nwant\nto\ncode!\n");
+
+    return 0;
+}
